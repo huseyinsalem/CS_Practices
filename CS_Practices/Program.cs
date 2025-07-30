96e386cea7d57719f73bfa4c52bfa80c3e0b4ec5
huseyinsalem <huseyinsalem65@gmail.com>
2025-07-30 14:24:37 +0300

    Add project files.

---
+++ CS_Practices/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Practices
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ürünün fiyatı: ");
+            double price = Convert.ToDouble(Console.ReadLine());
+
+
+            double KdvRate = 0.20;
+
+            double KdvPrice = price * KdvRate;
+
+            double TotalPrice = price + KdvPrice;
+
+            Console.WriteLine("Kdv tutarı: " + KdvPrice);
+            Console.WriteLine("Kdv'li Tutar: " + TotalPrice);
+
+            Console.Read();
+        }
+    }
+}
