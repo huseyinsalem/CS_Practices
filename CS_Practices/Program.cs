dc75f4a110e73997955f7ec72a9338b574886165
huseyinsalem <huseyinsalem65@gmail.com>
2025-07-30 15:25:22 +0300

    Added taximeter fare calculation based on distance

--- CS_Practices/Program.cs
+++ CS_Practices/Program.cs
@@ -10,18 +10,43 @@ namespace CS_Practices
     {
         static void Main(string[] args)
         {
-            Console.Write("Ürünün fiyatı: ");
-            double price = Convert.ToDouble(Console.ReadLine());
 
 
-            double KdvRate = 0.20;
+            #region kdv oranı 
+            //Console.Write("Ürünün fiyatı: ");
+            //double Price = Convert.ToDouble(Console.ReadLine());
 
-            double KdvPrice = price * KdvRate;
+            //double KdvRate = 0.20;
 
-            double TotalPrice = price + KdvPrice;
+            //double KdvPrice = Price * KdvRate;
 
-            Console.WriteLine("Kdv tutarı: " + KdvPrice);
-            Console.WriteLine("Kdv'li Tutar: " + TotalPrice);
+            //double TotalPrice = Price + KdvPrice;
+
+            //Console.WriteLine("Kdv oranı: " + KdvPrice);
+            //Console.WriteLine("Kdv'li fiyat: " + TotalPrice);
+            #endregion
+
+
+            #region Taksimetre hesaplama
+            double StartPrice = 25;
+            double PerKmPrice = 15.20;
+            double MinPrice = 150;
+
+            Console.Write("Mesafeyi giriniz: ");
+            double Km = Convert.ToDouble(Console.ReadLine());
+
+            double PayPrice = StartPrice + (Km * PerKmPrice);
+
+            PayPrice = PayPrice < MinPrice ? MinPrice : PayPrice;
+
+            Console.WriteLine("Taksi ücreti: " + PayPrice);
+
+            #endregion
+
+          
+
+
+
 
             Console.Read();
         }
